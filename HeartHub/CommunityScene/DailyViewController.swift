a00c1d177196ea9ac3443daa1403c2ffdc38bb15
lxodud <xodud1998223@naver.com>
2023-07-07 22:26:51 +0900

    feat: DailyCollectionView Layout 메서드 구현

--- HeartHub/CommunityScene/DailyViewController.swift
+++ HeartHub/CommunityScene/DailyViewController.swift
@@ -8,9 +8,41 @@
 import UIKit
 
 final class DailyViewController: UIViewController {
+    private let dailyCollectionView = UICollectionView(frame: .zero)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+    }
+}
+
+// MARK: Configure CollectionView
+extension DailyViewController {
+    private func configureDailyCollectionView()  {
+        dailyCollectionView.collectionViewLayout = generateCollectionViewLayout()
+        view.addSubview(dailyCollectionView)
+    }
+    
+    private func generateCollectionViewLayout() -> UICollectionViewCompositionalLayout {
+        let itemSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0),
+            heightDimension: .fractionalHeight(1.0)
+        )
+        let feedItem = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        let groupSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0),
+            heightDimension: .fractionalHeight(0.7)
+        )
+        let feedGroup = NSCollectionLayoutGroup
+            .vertical(
+                layoutSize: groupSize,
+                subitem: feedItem,
+                count: 1
+            )
+        
+        let section = NSCollectionLayoutSection(group: feedGroup)
+        
+        return UICollectionViewCompositionalLayout(section: section)
     }
 }
