426f107a482bed3d702ebc0c98b2a47250694d89
lxodud <xodud1998223@naver.com>
2023-07-07 22:04:21 +0900

    feat: DailyViewController 선언

---
+++ HeartHub/CommunityScene/DailyViewController.swift
@@ -0,0 +1,16 @@
+//
+//  DailyViewController.swift
+//  HeartHub
+//
+//  Created by 이태영 on 2023/07/06.
+//
+
+import UIKit
+
+final class DailyViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
