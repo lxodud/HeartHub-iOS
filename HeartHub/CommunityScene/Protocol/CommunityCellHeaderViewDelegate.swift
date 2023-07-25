0e47f6393619f4bb0da908d6b398c799b3938b87
lxodud <xodud1998223@naver.com>
2023-07-25 16:56:40 +0900

    feat: CommunityCellHeaderView, CommunityCellHeaderViewDelegate 구현

---
+++ HeartHub/CommunityScene/Protocol/CommunityCellHeaderViewDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  CommunityCellHeaderViewDelegate.swift
+//  HeartHub
+//
+//  Created by 이태영 on 2023/07/25.
+//
+
+import Foundation
+
+protocol CommunityCellHeaderViewDelegate: AnyObject {
+    func didTapPostOptionButton()
+}
