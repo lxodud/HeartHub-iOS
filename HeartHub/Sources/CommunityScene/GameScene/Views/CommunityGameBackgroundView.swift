be2a71dd8af41302405021757146afe54ad48d34
MinwooJe <gpatration77@gmail.com>
2023-08-02 21:12:27 +0900

    feat: CommunityGameBackgroundView 구현

---
+++ HeartHub/Sources/CommunityScene/GameScene/Views/CommunityGameBackgroundView.swift
@@ -0,0 +1,80 @@
+//
+//  CommunityGameBackgroundView.swift
+//  HeartHub
+//
+//  Created by 제민우 on 2023/08/02.
+//
+
+import UIKit
+
+class CommunityGameBackgroundView: UIView {
+
+    private var gameBackgroundImageView: UIImageView = {
+        var img = UIImageView()
+        img.contentMode = .scaleAspectFit
+        img.backgroundColor = .clear
+        img.image = UIImage(named: "GameBackground.png")
+        return img
+    }()
+    
+    private var gameFrontMountainImageView: UIImageView = {
+        var img = UIImageView()
+        img.contentMode = .scaleAspectFit
+        img.backgroundColor = .clear
+        img.image = UIImage(named: "GameMountainFront.png")
+        return img
+    }()
+    
+    private var missionTitleImageView: UIImageView = {
+        var img = UIImageView()
+        img.contentMode = .scaleAspectFit
+        img.backgroundColor = .clear
+        img.image = UIImage(named: "GameMissionTitle.png")
+        return img
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureSubview()
+        configureLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: Configure UI
+extension CommunityGameBackgroundView {
+    func configureSubview() {
+        [gameBackgroundImageView,
+         missionTitleImageView,
+         gameFrontMountainImageView,
+         ].forEach {
+            addSubview($0)
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
+    }
+    
+    func configureLayout() {
+        NSLayoutConstraint.activate([
+            // MARK: gmaeBackgroundImageView Constraints
+            gameBackgroundImageView.centerXAnchor.constraint(equalTo: centerXAnchor),
+            gameBackgroundImageView.centerYAnchor.constraint(equalTo: centerYAnchor),
+            gameBackgroundImageView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            gameBackgroundImageView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor),
+            
+            // MARK: missionTitleImageView Constraints
+            missionTitleImageView.centerXAnchor.constraint(equalTo: centerXAnchor),
+            missionTitleImageView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 29),
+            missionTitleImageView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 111),
+            
+            
+            // MARK: gameFrontMountainImageView Constraints
+            gameFrontMountainImageView.centerXAnchor.constraint(equalTo: centerXAnchor),
+            gameFrontMountainImageView.topAnchor.constraint(equalTo: missionTitleImageView.topAnchor, constant: 546),
+            gameFrontMountainImageView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor),
+        ])
+        
+    }
+}
