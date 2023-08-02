2c2dc5ec64bd4357f3ae431664d45bacce600bcb
MinwooJe <gpatration77@gmail.com>
2023-08-02 19:25:51 +0900

    feat: ProfileViewController 구현

---
+++ HeartHub/Sources/ProfileScene/Controllers/ProfileViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ProfileViewController.swift
+//  HeartHub
+//
+//  Created by 제민우 on 2023/08/02.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
