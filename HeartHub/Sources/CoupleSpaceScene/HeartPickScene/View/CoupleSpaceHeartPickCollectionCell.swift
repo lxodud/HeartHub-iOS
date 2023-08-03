090c90fec71642c1daba0013d56db69f07947844
lxodud <xodud1998223@naver.com>
2023-08-03 17:47:54 +0900

    feat: CoupleSpaceHeartPickCollectionCell 구현

---
+++ HeartHub/Sources/CoupleSpaceScene/HeartPickScene/View/CoupleSpaceHeartPickCollectionCell.swift
@@ -0,0 +1,59 @@
+//
+//  CoupleSpaceHeartPickCollectionCell.swift
+//  HeartHub
+//
+//  Created by 이태영 on 2023/08/03.
+//
+
+import UIKit
+
+final class CoupleSpaceHeartPickCollectionCell: UICollectionViewCell {
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        return imageView
+    }()
+    
+    private let label: UILabel = {
+        let label = UILabel()
+        return label
+    }()
+}
+
+// MARK: Public Interface
+extension CoupleSpaceHeartPickCollectionCell {
+    func configureContents() {
+        
+    }
+}
+
+// MARK: Configure UI
+extension CoupleSpaceHeartPickCollectionCell {
+    private func configureSubview() {
+        [imageView, label].forEach {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
+        
+        contentView.addSubview(imageView)
+        imageView.addSubview(label)
+    }
+    
+    private func configureLayout() {
+        let safeArea = contentView.safeAreaLayoutGuide
+        
+        NSLayoutConstraint.activate([
+            // MARK: imageView Constraints
+            imageView.topAnchor.constraint(
+                equalTo: safeArea.topAnchor
+            ),
+            imageView.leadingAnchor.constraint(
+                equalTo: safeArea.leadingAnchor
+            ),
+            imageView.trailingAnchor.constraint(
+                equalTo: safeArea.trailingAnchor
+            ),
+            imageView.bottomAnchor.constraint(
+                equalTo: safeArea.bottomAnchor
+            )
+        ])
+    }
+}
