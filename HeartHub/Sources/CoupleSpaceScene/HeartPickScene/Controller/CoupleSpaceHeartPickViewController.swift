1e49cf8c9d2ca596ad14a586d626f1c190bb6aeb
lxodud <xodud1998223@naver.com>
2023-08-03 16:21:39 +0900

    feat: CoupleSpaceHeartPickViewController 구현

---
+++ HeartHub/Sources/CoupleSpaceScene/HeartPickScene/Controller/CoupleSpaceHeartPickViewController.swift
@@ -0,0 +1,94 @@
+//
+//  CoupleSpaceHeartPickViewController.swift
+//  HeartHub
+//
+//  Created by 이태영 on 2023/08/03.
+//
+
+import UIKit
+
+final class CoupleSpaceHeartPickViewController: UIViewController {
+    private let pickCollectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.minimumLineSpacing = 0
+        layout.minimumInteritemSpacing = 0
+        let collectionView = UICollectionView(
+            frame: .zero,
+            collectionViewLayout: layout
+        )
+        return collectionView
+    }()
+    
+    private let pickTransitionButton: UIButton = {
+        let button = UIButton()
+        return button
+    }()
+}
+
+// MARK: Life Cycle
+extension CoupleSpaceHeartPickViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+// MARK: UICollectionView Delegate FlowLayout Implementation
+extension CoupleSpaceHeartPickViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(
+        _ collectionView: UICollectionView,
+        layout collectionViewLayout: UICollectionViewLayout,
+        sizeForItemAt indexPath: IndexPath
+    ) -> CGSize {
+        let width = view.frame.width / 2
+        let height = width
+        
+        return CGSize(width: width, height: height)
+    }
+}
+
+// MARK: Configure pickCollectionView
+extension CoupleSpaceHeartPickViewController {
+    private func configurePickCollectionView() {
+        pickCollectionView.delegate = self
+    }
+}
+
+// MARK: Configure UI
+extension CoupleSpaceHeartPickViewController {
+    private func configureSubview() {
+        [pickCollectionView, pickTransitionButton].forEach {
+            view.addSubview($0)
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
+    }
+    
+    private func configureLayout() {
+        let safeArea = view.safeAreaLayoutGuide
+        
+        NSLayoutConstraint.activate([
+            // MARK: pickCollectionView Constraints
+            pickCollectionView.topAnchor.constraint(
+                equalTo: safeArea.topAnchor
+            ),
+            pickCollectionView.leadingAnchor.constraint(
+                equalTo: safeArea.leadingAnchor
+            ),
+            pickCollectionView.trailingAnchor.constraint(
+                equalTo: safeArea.leadingAnchor
+            ),
+            pickCollectionView.bottomAnchor.constraint(
+                equalTo: view.bottomAnchor
+            ),
+            
+            // MARK: pickTransitionButton Constraints
+            pickTransitionButton.trailingAnchor.constraint(
+                equalTo: safeArea.trailingAnchor,
+                constant: -21
+            ),
+            pickTransitionButton.bottomAnchor.constraint(
+                equalTo: safeArea.bottomAnchor,
+                constant: -21
+            ),
+        ])
+    }
+}
