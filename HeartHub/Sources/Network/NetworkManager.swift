074194ba883365301013f0329380662f474693dc
lxodud <xodud1998223@naver.com>
2023-08-10 17:46:28 +0900

    feat: NetworkManager, Requestable 선언

---
+++ HeartHub/Sources/Network/NetworkManager.swift
@@ -0,0 +1,24 @@
+//
+//  NetworkManager.swift
+//  HeartHub
+//
+//  Created by 이태영 on 2023/08/10.
+//
+
+import Foundation
+
+protocol NetworkManager {
+    func request(
+        endpoint: Requestable,
+        completion: @escaping (Result<Data, Error>) -> Void
+     )
+}
+
+final class DefaultNetworkManager: NetworkManager {
+    func request(
+        endpoint: Requestable,
+        completion: @escaping (Result<Data, Error>
+        ) -> Void) {
+        
+    }
+}
---
+++ HeartHub/Sources/Network/Requestable.swift
@@ -0,0 +1,10 @@
+//
+//  Requestable.swift
+//  HeartHub
+//
+//  Created by 이태영 on 2023/08/10.
+//
+
+import Foundation
+
+protocol Requestable { }
