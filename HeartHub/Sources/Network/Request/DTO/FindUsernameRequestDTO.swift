086389ef0e63cca56e8b2f4f0850354746116904
lxodud <xodud1998223@naver.com>
2023-08-13 01:23:04 +0900

    chore: 폴더 위치 수정

---
+++ HeartHub/Sources/Network/Request/DTO/FindUsernameRequestDTO.swift
@@ -0,0 +1,12 @@
+//
+//  FindUsernameRequestDTO.swift
+//  HeartHub
+//
+//  Created by 이태영 on 2023/08/13.
+//
+
+import Foundation
+
+struct FindUsernameRequestDTO: Encodable {
+    let email: String
+}
