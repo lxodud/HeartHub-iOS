0423e4fad9e6f41912a6f46736357cea166edb42
lxodud <xodud1998223@naver.com>
2023-08-12 23:45:39 +0900

    refactor: LoginRequest로 네이밍 수정

--- HeartHub/Sources/Network/LoginRequest.swift
+++ HeartHub/Sources/Network/LoginRequest.swift
@@ -7,20 +7,20 @@
 
 import Foundation
 
-struct LoginEndpoint {
-    static func makeEmailCheckEndpoint(of email: String) -> Requestable {
+struct LoginRequest {
+    static func makeEmailCheckRequest(of email: String) -> Requestable {
         return Request(
             path: "api/check/email/" + email
         )
     }
     
-    static func makeIdCheckApiEndpoint(of id: String) -> Requestable {
+    static func makeIdCheckApiRequest(of id: String) -> Requestable {
         return Request(
             path: "api/check/username/" + id
         )
     }
     
-    static func makeVerificateEmailEndPoint(of email: String) -> Requestable {
+    static func makeVerificateEmailRequest(of email: String) -> Requestable {
         return Request(
             httpMethod: .post,
             path: "api/join"
