4ebf3a02e2d0243c525d06e758ca096d353d7d4b
MinwooJe <gpatration77@gmail.com>
2023-08-16 15:03:01 +0900

    feat: AddPostView, AddPostViewController 구현

---
+++ HeartHub/Sources/CommunityScene/AddPostScene/Controllers/AddPostViewController.swift
@@ -0,0 +1,17 @@
+//
+//  AddPostViewController.swift
+//  HeartHub
+//
+//  Created by 제민우 on 2023/08/16.
+//
+
+import UIKit
+
+final class AddPostViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
---
+++ HeartHub/Sources/CommunityScene/AddPostScene/Views/AddPostView.swift
@@ -0,0 +1,77 @@
+//
+//  AddPostView.swift
+//  HeartHub
+//
+//  Created by 제민우 on 2023/08/16.
+//
+
+import UIKit
+
+final class AddPostView: UIView {
+    private let postImageView: UIImageView = {
+       let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.image = UIImage(named: "TestImage")
+        return imageView
+    }()
+    
+    private var addPostProfileView = CommunityProfileView()
+    
+    private let addPostTextField: UITextField = {
+       let textField = UITextField()
+        textField.font = UIFont(name: "Pretendard-Regular", size: 16)
+        textField.attributedPlaceholder = NSAttributedString(
+            string: "문구를 입력해주세요",
+            attributes: [NSAttributedString.Key.foregroundColor: #colorLiteral(red: 0.07, green: 0.07, blue: 0.07, alpha: 0.5)]
+        )
+        textField.textColor = .black
+        textField.autocapitalizationType = .none
+        textField.autocorrectionType = .no
+        textField.spellCheckingType = .no
+        return textField
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        configureSubviews()
+        configureLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: Configure Layout
+extension AddPostView {
+    
+    private func configureSubviews() {
+        [postImageView,
+         addPostProfileView,
+         addPostTextField].forEach {
+            addSubview($0)
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
+    }
+    
+    private func configureLayout() {
+        let safeArea = safeAreaLayoutGuide
+        NSLayoutConstraint.activate([
+            // MARK: postImageView Constraints
+            postImageView.centerXAnchor.constraint(equalTo: centerXAnchor),
+            postImageView.topAnchor.constraint(equalTo: safeArea.topAnchor),
+            postImageView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
+
+            // MARK: addPostProfileView Constraints
+            addPostProfileView.centerXAnchor.constraint(equalTo: centerXAnchor),
+            addPostProfileView.topAnchor.constraint(equalTo: postImageView.bottomAnchor),
+            addPostProfileView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
+
+            // MARK: addPostTextField Constraints
+            addPostTextField.centerXAnchor.constraint(equalTo: centerXAnchor),
+            addPostTextField.topAnchor.constraint(equalTo: addPostProfileView.bottomAnchor),
+            addPostTextField.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor),
+            addPostTextField.leadingAnchor.constraint(equalTo: leadingAnchor),
+        ])
+    }
+}
